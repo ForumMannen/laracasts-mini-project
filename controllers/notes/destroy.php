b792247fa8138ef63064e55ca61907b2210a1f20
Linus Lundell <linus@humbleguys.se>
2024-07-12 14:28:15 +0200

    Now the delete works aswell

---
+++ controllers/notes/destroy.php
@@ -0,0 +1,18 @@
+<?php
+
+use Core\App;
+
+$db = App::container()->resolve(\Core\Database::class);
+
+$currentUserId = 1;
+
+$note = $db->query('select * from notes where id = :id', ['id' => $_POST['id']])->findOrFail();
+
+authorize($note['user_id'] === $currentUserId);
+
+$db->query('delete from notes where id = :id', [
+    'id' => $_GET['id']
+]);
+
+header('location: /notes');
+exit();
--- controllers/notes/show.php
+++ controllers/notes/show.php
@@ -4,26 +4,13 @@ use Core\Database;
 
 $config = require base_path('config.php');
 $db = new Database($config['database']);
-$currentUserId = 25;
+$currentUserId = 1;
 
-if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
+$note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
 
-    authorize($note['user_id'] === $currentUserId);
+authorize($note['user_id'] === $currentUserId);
 
-    $db->query('delete from notes where id = :id', [
-        'id' => $_GET['id']
-    ]);
-
-    header('location: /notes');
-    exit();
-} else {
-    $note = $db->query('select * from notes where id = :id', ['id' => $_GET['id']])->findOrFail();
-
-    authorize($note['user_id'] === $currentUserId);
-
-    view("notes/show.view.php", [
-        'heading' => 'Note',
-        'note' => $note
-    ]);
-}
+view("notes/show.view.php", [
+    'heading' => 'Note',
+    'note' => $note
+]);
